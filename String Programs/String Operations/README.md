7fcdef863abb744f60dfe37001769a5b4a0ed5e5
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-17 21:12:58 +0530

    Add Java program to split string by spaces

---
+++ String Programs/String Operations/README.md
@@ -0,0 +1,9 @@
+# SpaceIn
+
+A simple Java program that processes a string and prints each word on a separate line by detecting spaces in the input.
+
+## Example
+
+### Input
+```text
+Hello World Java
---
+++ String Programs/String Operations/SpaceIn.java
@@ -0,0 +1,17 @@
+
+package String;
+import java.util.Scanner;
+
+public class Spaceln {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                System.out.println();
+            } else {
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+}
